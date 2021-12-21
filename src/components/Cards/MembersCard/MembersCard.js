bfcf2cc9d30006c7a7b59e9651ea0458a7fa6aad
kaedejima <frogtoadie@gmail.com>
2021-12-21 23:05:40 +0900

    fixstyle: MembersCard

--- src/components/Cards/MembersCard/MembersCard.js
+++ src/components/Cards/MembersCard/MembersCard.js
@@ -13,9 +13,9 @@ export const MembersCard = () => {
         <div>
             <h1 className='title'>Members</h1>
             <h2 className='sub-title'>TeamName Lead</h2>
-            <Container className='leader justify-content-center'>
+            <Container fluid='md' className='leader justify-content-center'>
                 <Link to='members'>
-                    <Card className='card-frame col-md-auto'>
+                    <Card className='card-frame col-md-auto '>
                         <Card.Img id='card-img' variant="top" src={ Image } />
                         <Card.Body>
                             <Card.Title id='title'>FirstName LastName</Card.Title>
@@ -27,17 +27,17 @@ export const MembersCard = () => {
                 </Link>
             </Container>
 
-            <Container className='members justify-content-center'> {/* 'MembersCard'16枚分を1つのオブジェクトとしてグループ化 */}
-                <Row xs={1} md={4} className="g-4">{
+            <Container fluid='md' className='members justify-content-center'> {/* 'MembersCard'16枚分を1つのオブジェクトとしてグループ化 */}
+                <Row xs={2} md={4} className="g-4">{
                     Array.from({ length: 16 }).map((_, idx) => (
                         <Col>
                             <Link to='members'>
-                                <Card className='card-frame'>
+                                <Card className='card-frame mx-auto'>
                                     <Card.Img id='card-img' variant="top" src={ Image } />
                                     <Card.Body>
                                     <Card.Title id='title'>FirstName LastName</Card.Title>
                                     <Card.Text>
-                                        
+
                                     </Card.Text>
                                     </Card.Body>
                                 </Card>
