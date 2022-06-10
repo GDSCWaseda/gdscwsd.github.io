b5b23109e0e5ae4e9e45970cce9557d92909e143
irfanns <irfanns@pop-os.localdomain>
2022-06-10 21:28:34 +0900

    Sorry, forgot some files. They are all added now. Check previous commit history for the edited files

---
+++ src/frontend-page/MembersPage.tsx
@@ -0,0 +1,11 @@
+import './MembersPage.css'
+
+import React from 'react'
+
+export const MembersPage = () => {
+  return (
+    <div>
+      <div className="test">Members Page</div>
+    </div>
+  )
+}
