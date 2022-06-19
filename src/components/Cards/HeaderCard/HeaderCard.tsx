2c6f777ddd3331300b731c49726eee4ab8aee62d
cn-eveer <sienreeve46@gmail.com>
2022-06-20 01:57:25 +0900

    apply format change

--- src/components/Cards/HeaderCard/HeaderCard.tsx
+++ src/components/Cards/HeaderCard/HeaderCard.tsx
@@ -12,22 +12,26 @@ import { HeaderCardProps } from '~/types/index'
 export const HeaderCard: React.FC<{ props: HeaderCardProps }> = ({ props }) => {
   return (
     <Card className="header-card">
-      <Card.Title className="header-card__header-title">{props.headTitle}</Card.Title>
+      <Card.Title className="header-card__header-title">
+        {props.headTitle}
+      </Card.Title>
       <div className="header-card__container">
         <Row>
-          <Col xs={2} style={{ textAlign: "end", alignSelf: "center" }}>
+          <Col xs={2} style={{ textAlign: 'end', alignSelf: 'center' }}>
             <img src={LogoLeft} className="header-card__logo-left" />
           </Col>
           <Col xs={8}>
             <Card.Text className="header-card__title">{props.title}</Card.Text>
           </Col>
-          <Col xs={2} style={{ alignSelf: "center" }}>
+          <Col xs={2} style={{ alignSelf: 'center' }}>
             <img src={LogoRight} className="header-card__logo-right" />
           </Col>
         </Row>
       </div>
       {props.content.split('\n').map((text, key) => (
-        <p key={key} className="header-card__contents">{text}</p>
+        <p key={key} className="header-card__contents">
+          {text}
+        </p>
       ))}
     </Card>
   )
