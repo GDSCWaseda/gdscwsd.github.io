a1e5f02220e33c590abf2b0e9e0f81362102b62b
cn-eveer <sienreeve46@gmail.com>
2022-06-23 01:20:29 +0900

    add mui theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { muiTheme } from 'storybook-addon-material-ui'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +9,7 @@ export const parameters = {
     }
   }
 }
+
+export const decorators = [
+	muiTheme()
+];
